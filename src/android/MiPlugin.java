6f691b0c670dae515710b8c0d98f83e9a12294c9
Chuck-1992 <charliegithub2020@gmail.com>
2024-06-17 18:14:58 -0500

    Cambios en el envio de parametros

--- src/android/MiPlugin.java
+++ src/android/MiPlugin.java
@@ -1,26 +1,26 @@
 package org.example.miplugin;
 
-import org.apache.cordova.*;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 
 public class MiPlugin extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if ("saludar".equals(action)) {
-            this.saludar(args, callbackContext);
+        if (action.equals("coolMethod")) {
+            String message = args.getString(0);
+            this.coolMethod(message, callbackContext);
             return true;
         }
         return false;
     }
 
-    private void saludar(JSONArray args, CallbackContext callbackContext) {
-        try {
-            String nombre = args.getString(0);
-            String mensaje = "Hola " + nombre;
-            callbackContext.success(mensaje);
-        } catch (JSONException e) {
-            callbackContext.error("Error al leer el nombre");
+    private void coolMethod(String message, CallbackContext callbackContext) {
+        if (message != null && message.length() > 0) {
+            callbackContext.success("Hola, " + message);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
         }
     }
 }
--- www/MiPlugin.js
+++ www/MiPlugin.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
+exports.saludar = function (arg0, success, error) {
     exec(success, error, 'MiPlugin', 'coolMethod', [arg0]);
 };
